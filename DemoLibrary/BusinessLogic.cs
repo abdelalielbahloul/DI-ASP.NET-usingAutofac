a1dab3d8fce8ee6b36efb07e5c023eb74066a709
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-30 13:02:11 +0100

    feat(BasicProject)
    Add a basic project without implementation of DI

---
+++ DemoLibrary/BusinessLogic.cs
@@ -0,0 +1,25 @@
+﻿using DemoLibrary.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class BusinessLogic 
+    { 
+        public void ProcessData()
+        {
+            Logger logger = new Logger();
+            DataAccess dataAccess = new DataAccess();
+
+
+            logger.Log("Starting the processing of data");
+            Console.WriteLine("Processing of data");
+            dataAccess.LoadData();
+            dataAccess.SaveData("processingInfo");
+            logger.Log("Finished processing of data");
+        }
+    }
+}
---
+++ DemoLibrary/Utilities/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Utilities
+{
+    public class DataAccess
+    {
+        public void LoadData()
+        {
+            Console.WriteLine("Loading data");
+        }
+
+        public void SaveData(string name)
+        {
+            Console.WriteLine($"Saving { name }");
+        }
+
+    }
+}

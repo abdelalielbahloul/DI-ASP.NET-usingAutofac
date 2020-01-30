4715d75e921c12f88b8da41f7110c061d109e274
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-30 14:56:40 +0100

    feat(Container)
    add the container to our application &
    we built an application open to extension and closed to modification with DI using Autofac

--- ConsoleUI/ContainerConfig.cs
+++ ConsoleUI/ContainerConfig.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using DemoLibrary;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace ConsoleUI
@@ -10,6 +13,19 @@ namespace ConsoleUI
         public static IContainer Config()
         {
             var builder = new ContainerBuilder();
+
+            /***
+             * here we associate each interface with her implementation class
+             */
+            builder.RegisterType<Application>().AsSelf().As<IApplication>();
+            builder.RegisterType<BusinessLogic>().AsSelf().As<IBusinessLogic>();
+            /***
+             * here we want to get all classes and interfaces in the Namespace DemoLibrary.Utilities
+             * and associate each class with its interface
+             */
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(t => t.Namespace.Contains("Utilities"))
+                .As(c => c.GetInterfaces().FirstOrDefault(i => i.Name == $"I{c.Name}"));
 
             return builder.Build();
         }
